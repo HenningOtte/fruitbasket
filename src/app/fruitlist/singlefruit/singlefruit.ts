57697f58dc853cc8580189142d07eefa97cd1512
HenningOtte <HenningOtte91@gmail.com>
2025-11-07 23:21:54 +0100

    feat: forward input from SingleFruit to FruitList via @Output and service
    - Input value from singlefruit.html emitted through @Output() fruitname
    - Parent FruitList component receives event and passes it to FruitListData service

--- src/app/fruitlist/singlefruit/singlefruit.ts
+++ src/app/fruitlist/singlefruit/singlefruit.ts
@@ -1,18 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-single-fruit',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './singlefruit.html',
   styleUrl: './singlefruit.scss',
 })
 export class SingleFruit {
-  getStarType(stars: number, count: number): 'half' | 'full' | 'empty' {
-    if (stars >= count) return 'full';
-    if (stars >= count - 0.5) return 'half';
-    return 'empty';
-  }
-
   @Input() fruit = {
     name: 'Apfel',
     img: 'apple.png',
@@ -25,5 +20,21 @@ export class SingleFruit {
       { name: 'Arne P.', text: 'nicht so meins' },
     ],
   };
+
+  InputData = '';
+
   @Input() fruitNumber: number = 0;
+
+  @Output() fruitname = new EventEmitter<string>();
+
+  sendInput() {
+    this.fruitname.emit(this.InputData);
+    this.InputData = '';
+  }
+
+  getStarType(stars: number, count: number): 'half' | 'full' | 'empty' {
+    if (stars >= count) return 'full';
+    if (stars >= count - 0.5) return 'half';
+    return 'empty';
+  }
 }
